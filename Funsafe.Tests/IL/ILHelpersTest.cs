0c1d8025411b2840c838c6ad50bf252a8d65dcb5
Romain Verdier <romain.verdier@gmail.com>
2015-07-22 21:55:52 +0200

    Add basic tests for ILHelpers

--- Funsafe.Tests/IL/ILHelpersTest.cs
+++ Funsafe.Tests/IL/ILHelpersTest.cs
@@ -1,13 +1,86 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace Funsafe.Tests.IL
 {
     [TestFixture]
-    public class ILHelpersTest
+    public unsafe class ILHelpersTest
     {
-        [Test, Ignore("not yet ready")]
-        public void Should_just_work_lol()
+        [Test]
+        public void Should_move_cursor_forward_and_return_read_value()
         {
+            var guid = Guid.NewGuid();
+            byte* bufferPtr = stackalloc byte[1024];
+
+            var cursor = bufferPtr;
+            *(Guid*) cursor = guid;
+
+            var value = ILHelpers.Read3<Guid>(ref cursor);
+
+            Assert.AreEqual(value, guid);
+            Assert.AreEqual((int) cursor, (int) bufferPtr + sizeof (Guid));
+        }
+
+        [Test]
+        public void Should_read_and_move_cursor_forward()
+        {
+            var guid = Guid.NewGuid();
+            byte* bufferPtr = stackalloc byte[1024];
+
+            var cursor = bufferPtr;
+            *(Guid*) cursor = guid;
+
+            Guid value;
+            ILHelpers.Read(ref cursor, out value);
+
+            Assert.AreEqual(value, guid);
+            Assert.AreEqual((int) cursor, (int) bufferPtr + sizeof (Guid));
+        }
+
+        [Test]
+        public void Should_read_and_return_new_cursor_value()
+        {
+            var guid = Guid.NewGuid();
+            byte* bufferPtr = stackalloc byte[1024];
+
+            var cursor = bufferPtr;
+            *(Guid*) cursor = guid;
+
+            Guid value;
+            cursor = ILHelpers.Read2(cursor, out value);
+
+            Assert.AreEqual(value, guid);
+            Assert.AreEqual((int) cursor, (int) bufferPtr + sizeof (Guid));
+        }
+
+        [Test]
+        public void Should_write_and_move_cursor_forward()
+        {
+            var guid = Guid.NewGuid();
+            byte* bufferPtr = stackalloc byte[1024];
+            var cursor = bufferPtr;
+
+            ILHelpers.Write(ref cursor, ref guid);
+
+            var value = *(Guid*) bufferPtr;
+
+            Assert.AreEqual(value, guid);
+            Assert.AreEqual((int) cursor, (int) bufferPtr + sizeof (Guid));
+        }
+
+        [Test]
+        public void Should_write_and_return_new_cursor_value()
+        {
+            var guid = Guid.NewGuid();
+            byte* bufferPtr = stackalloc byte[1024];
+            var cursor = bufferPtr;
+
+            cursor = ILHelpers.Write2(cursor, ref guid);
+
+            var value = *(Guid*) bufferPtr;
+
+            Assert.AreEqual(value, guid);
+            Assert.AreEqual((int) cursor, (int) bufferPtr + sizeof (Guid));
         }
     }
 }
